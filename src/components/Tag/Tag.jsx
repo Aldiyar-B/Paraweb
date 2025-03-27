c3dea9e76a68d36c6381ac53043eb9568943798e
Aldiyar-B <aimamadiev@mail.ru>
2025-03-27 07:53:41 +0700

    Tag сделал по Бэм и rem

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import styles from './Tag.module.scss';
 
-const Tag = ({ label, subtitle, color = 'default' }) => {
+const Tag = ({ label, color = 'default' }) => {
   if (!label) return null;
 
   return (
-    <div className={`${styles.tag} ${styles[color]}`}>
-      {label && <span className={styles.label}>{label}</span>}
-      {/* {subtitle && <span className={styles.subtitle}>{subtitle}</span>} */}
+    <div className={`${styles.tag} ${styles[`tag--${color}`]}`}>
+      {label && <span className={styles.tag__label}>{label}</span>}
     </div>
   );
 };
